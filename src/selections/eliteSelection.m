de48104fe852c1e851c4cfb50dec09acab1f76e9
AngieArlanti <angeles.arlanti@mercadolibre.com>
2017-05-29 20:58:18 -0300

    Elite Selection Done Closes #15

--- src/selections/eliteSelection.m
+++ src/selections/eliteSelection.m
@@ -1,9 +1,11 @@
-% TODO
+function selection = eliteSelection(population, relativeFitness,k)
+#function selection = eliteSelection(population={{[1,2,3,4,6],'x1'} {[7,8,3,4,6],'x2'} {[8,7,30,4,6],'x3'} {[1,2,3,4,6],'x4'} {[1,2,3,4,6],'x5'}}, relativeFitness=[0.086, 0.171, 0.314, 0.400, 0.029], k=3)
+	selection = {};
+	
+	[s, i] = sort(relativeFitness,'descend');
+	indexes = i(1:k)
 
-function selection = eliteSelection(population, relativeFitnesses, configuration)
-%eliteSelection - Selects future parents using elite selection
-%
-% Syntax: selection = eliteSelection(population, relativeFitnesses, configuration)
-%
-  
-end
+	for i=1:k	
+		selection{i} = population{indexes(i)};
+	endfor
+endfunction
