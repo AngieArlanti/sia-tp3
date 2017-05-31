5800e112d9be605aba5b4839a9afe7b4a954021b
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-05-31 18:59:40 -0300

    - Added m in config and tournament selection

--- src/selections/rankingSelection.m
+++ src/selections/rankingSelection.m
@@ -1,9 +1,6 @@
-% TODO
-
+#rankingSelection - Selects future parents using ranking selection
 function selection = rankingSelection(population, relativeFitnesses, configuration)
-%rankingSelection - Selects future parents using ranking selection
-%
-% Syntax: selection = rankingSelection(population, relativeFitnesses, configuration)
-%
+
+
   
 end
