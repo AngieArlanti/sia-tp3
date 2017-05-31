6adf9e48329f070411b8719a748fd0b927c73b00
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-05-31 20:10:53 -0300

    - Probabilistic tournament implemented and tested

--- src/selections/deterministicTournamentSelection.m
+++ src/selections/deterministicTournamentSelection.m
@@ -1,15 +1,15 @@
 %deterministicTournamentSelection - Selects future parents using deterministicTournament selection
-%function selection = deterministicTournamentSelection(population, relativeFitnesses, configuration)
-	function selection = deterministicTournamentSelection(population={{[1,2,3,4,6],'x1'} {[7,8,3,4,6],'x2'} {[8,7,30,4,6],'x3'} {[1,2,3,4,6],'x4'} {[1,2,3,4,6],'x5'}}, relativeFitness=[0.086, 0.171, 0.314, 0.400, 0.029], parseConfigurationFile('./configuration.txt'))
+function selection = deterministicTournamentSelection(population, relativeFitnesses, configuration)
+	%function selection = deterministicTournamentSelection(population={{[1,2,3,4,6],'x1'} {[7,8,3,4,6],'x2'} {[8,7,30,4,6],'x3'} {[1,2,3,4,6],'x4'} {[1,2,3,4,6],'x5'}}, relativeFitness=[0.086, 0.171, 0.314, 0.400, 0.029], parseConfigurationFile('./configuration.txt'))
 	% Repeat deterministic tournament selection k times
  	for i = 1:configuration.k
  		% Choose m individuals randomly
  		randomIndexes = randperm(length(population));
- 		pop = [];
+ 		pop = {};
  		fit = [];
 		for j = 1:configuration.m
-			pop(j)={population{indexes(j)}};
-			fit(j)={relativeFitnesses{indexes(j)}};
+			pop(j)={population{randomIndexes(j)}};
+			fit(j)=relativeFitnesses(randomIndexes(j));
 		end
 		selection{i} = eliteSelection(pop,fit,1);
 	end
