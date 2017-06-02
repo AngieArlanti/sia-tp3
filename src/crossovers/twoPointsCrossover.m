bb3bbbd51e0a31b15fb91200e56384c182f9ee4c
Santiago Ocamica <santi6982@gmail.com>
2017-06-02 11:06:11 -0300

    Implements anular and twoPoints crossover

--- src/crossovers/twoPointsCrossover.m
+++ src/crossovers/twoPointsCrossover.m
@@ -6,5 +6,24 @@ function children = twoPointsCrossover(parent1, parent2, configuration)
 % Syntax: children = twoPointsCrossover(parent1, parent2, configuration)
 %
 % Long description
+  genes1 = [parent1{1} parent1{2}];
+  genes2 = [parent2{1} parent2{2}];
+  chromosomeLength = 6;
   
+  locus1 = ceil(rand * (chromosomeLength - 1)); % -1 because it can't be the last one
+  locus2 = ceil(rand * (chromosomeLength - locus1)) + locus1; % has to be between locus1 and chromosomeLength
+  
+  resultGenes1 = [genes1(locus1:locus2) genes2(1:locus1) genes2(locus2:chromosomeLength)];
+  resultGenes2 = [genes2(locus1:locus2) genes1(1:locus1) genes1(locus2:chromosomeLength)];
+
+  children = {
+    {
+      resultGenes1(1:5),
+      resultGenes1(6)
+    },
+    {
+      resultGenes2(1:5),
+      resultGenes2(6)
+    }
+  };
 end
