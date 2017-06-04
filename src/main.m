e7498751ac4878fa0cdef3bdeadb7a1c3fe30bd2
Agop Hurmuz <agophurmuz@gmail.com>
2017-06-04 17:53:39 -0300

    added average

--- src/main.m
+++ src/main.m
@@ -2,6 +2,7 @@ function [generation maxFitness maxFitnessIndividual] = main(items = readItems, 
   addpath(genpath('.'));
   temperature = 1;
   maxFitnesses = [];
+  averageFitnesses = [];
   % struct de structs de vector de ids y la h (de cada item)
   population = generatePopulation(configuration.N);
   populationFitnesses = calculateAllFitness(population, items, configuration);
@@ -20,11 +21,14 @@ function [generation maxFitness maxFitnessIndividual] = main(items = readItems, 
     [maxFitness maxFitnessIndex] = max(populationFitnesses);
     maxFitnesses = [maxFitnesses maxFitness];
     maxFitnessIndividual = population{maxFitnessIndex};
+    averageFitnesses = [averageFitnesses sum(populationFitnesses)/length(populationFitnesses)];
     figure(1);
     drawBestIndividualItems(maxFitnessIndividual, items);
     figure(2);
     newplot;
-    plot(maxFitnesses);
+    plot(maxFitnesses, 'r');
+    hold on;
+    plot(averageFitnesses, 'b');
     xlim([1, 100]);
     ylim([0, 40]);
     refresh;
