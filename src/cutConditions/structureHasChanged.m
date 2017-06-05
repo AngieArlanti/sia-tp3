a6acc354205b15f73ebdcbc94a49e46765cc2ecd
Agop Hurmuz <agophurmuz@gmail.com>
2017-06-05 16:39:39 -0300

    arreglado el corte por estructura

--- src/cutConditions/structureHasChanged.m
+++ src/cutConditions/structureHasChanged.m
@@ -2,12 +2,12 @@
 function boolean = structureHasChanged(population, previousPopulation ,configuration)
 %Test
 %function boolean = structureHasChanged(population, previousPopulation ,structureMaxMatchPercentage)
-  maxAmountOfEqualsIndividuals = ceil(length(previousPopulation)*configuration.structureMaxMatchPercentage);
+  maxAmountOfEqualsIndividuals = ceil(length(population)*configuration.structureMaxMatchPercentage);
   %Test
   %maxAmountOfEqualsIndividuals = ceil(length(population)*structureMaxMatchPercentage);
   equalIndividuals = 0;
   for i=1:length(previousPopulation)
-    equalIndividuals += population(i) == previousPopulation(i);
+    equalIndividuals += population{i}{1} == previousPopulation{i}{1};
 	if(equalIndividuals == maxAmountOfEqualsIndividuals)
       break;
 	end
