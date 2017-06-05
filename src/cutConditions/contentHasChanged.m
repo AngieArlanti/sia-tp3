9ce0407f227d9152e23686780445087eb7dde4d7
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2017-06-04 22:06:45 -0300

    - Retrieve param from config

--- src/cutConditions/contentHasChanged.m
+++ src/cutConditions/contentHasChanged.m
@@ -1,7 +1,8 @@
 function boolean = contentHasChanged(maxFitnesses,configuration)
 %Test
 %function boolean = contentHasChanged(maxFitnesses,contentPreviousIndexToCompare)
-  if(length(maxFitnesses) - contentPreviousIndexToCompare > 0)
+ %if(length(maxFitnesses) - configuration.contentPreviousIndexToCompare > 0)
+  if(length(maxFitnesses) - configuration.contentPreviousIndexToCompare > 0)
     boolean = maxFitnesses(end) != maxFitnesses(configuration.contentPreviousIndexToCompare);
     %Test
     %boolean = maxFitnesses(end) != maxFitnesses(end-contentPreviousIndexToCompare);
