1401475612bdd6fc4e9029bd7f54cc446b865bc5
Agop Hurmuz <agophurmuz@gmail.com>
2017-06-06 21:24:09 -0300

    arreglado cruza de 2 puntos

--- src/crossovers/twoPointsCrossover.m
+++ src/crossovers/twoPointsCrossover.m
@@ -10,11 +10,25 @@ function children = twoPointsCrossover(parent1, parent2, configuration)
   genes2 = [parent2{1} parent2{2}];
   chromosomeLength = 6;
   
-  locus1 = ceil(rand * (chromosomeLength - 1)); % -1 because it can't be the last one
-  locus2 = ceil(rand * (chromosomeLength - locus1)) + locus1; % has to be between locus1 and chromosomeLength
-  
-  resultGenes1 = [genes1(locus1:locus2) genes2(1:locus1) genes2(locus2:chromosomeLength)];
-  resultGenes2 = [genes2(locus1:locus2) genes1(1:locus1) genes1(locus2:chromosomeLength)];
+  #locus1 = ceil(rand * (chromosomeLength - 1)); % -1 because it can't be the last one
+  #locus2 = ceil(rand * (chromosomeLength - locus1)) + locus1; % has to be between locus1 and chromosomeLength
+  locus1 = 0;
+  locus2 = 0;
+  while (locus1 >= locus2)
+    locus1 = ceil(rand*(chromosomeLength-1));
+    locus2 = ceil(rand*(chromosomeLength-1));
+  end
+
+  #resultGenes1 = [genes1(locus1:locus2) genes2(1:locus1) genes2(locus2:chromosomeLength)];
+  #resultGenes2 = [genes2(locus1:locus2) genes1(1:locus1) genes1(locus2:chromosomeLength)];
+  aux = locus1+locus2-1;
+  if(aux >= chromosomeLength)
+    resultGenes1 = [genes1(1:locus1-1) genes2(locus1:chromosomeLength)];
+    resultGenes2 = [genes2(1:locus1-1) genes1(locus1:chromosomeLength)];
+  else
+    resultGenes1 = [genes1(1:locus1-1) genes2(locus1:locus1+locus2-1) genes1(locus1+locus2:chromosomeLength)];
+    resultGenes2 = [genes2(1:locus1-1) genes1(locus1:locus1+locus2-1) genes2(locus1+locus2:chromosomeLength)];
+  end 
 
   children = {
     {
